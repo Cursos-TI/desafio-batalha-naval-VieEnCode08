6fef9bb99c66bb7ccef66ac743a2a550a8666e6c
VieEnCode08 <gabrielfontenelle04@gmail.com>
2025-10-17 17:45:27 +0000

    Criado menu. Criado a lógica que coloca os 0 no tabuleiro. Desenvolvi a lógica para colocar os na- vios no tabuleiro.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,73 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+    int main () {
+        // Variavel que representa o tabuleiro 
+        int agua [10] [10] = {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+ 
+        // Linha Horizontal do Tabuleiro
+        char tabela [10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+        // Linha vertical do Tabuleiro
+        int lateral [10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+        // Navio Horizontal
+        int tamanhoNavio = 3;
+        int coluna1= 5;
+        int linha1 = 3;
+        
+        // Navio Vertical
+        int coluna2 = 3;
+        int linha2 = 6;
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+        // Mini Menu
+        printf ("=====Bem-Vindo ao Jogo Batalha Naval=====\n");
+        printf ("\n");
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+        for (int i = 0; i < 10; i++) { // Responsavel por percorrer as linha 
+           for (int j = 0; j < 10; j++) { // Responsavel por percorrer as colunas 
+                if (i == linha1 && j >= coluna1 && j < coluna1 + tamanhoNavio) {  // Lógica responsavel por colocar o navio na horizontal
+                    agua[i][j] = 3; // Faz imprimir o numero "3" no tabuleiro caso a lógica seja verdadeira
+                } else {
+                    agua [i][j]; // Imprime "0" se a lógica for falsa
+                }
+           }
+        }
+
+        for (int i = 0; i < 10; i++) { // Responsavel por percorrer as linha 
+            for (int j = 0; j < 10; j++) { // Responsavel por percorrer as colunas 
+                if (j == coluna2 && i >= linha2 && i < linha2 + tamanhoNavio) {  // Lógica responsavel por colocar o navio na vertical
+                   agua[i] [j] = 3; // Faz imprimir o numero "3" no tabuleiro caso a lógica seja verdadeira
+                } else {
+                    agua[i] [j]; // Imprime "0" se a lógica for falsa
+                }
+            }
+        }
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+        printf ("   "); // Pra dar um espaço entre as linhas e colunas
+        
+        for (int i = 0; i < 10; i++) { // Esse for é responsavel por imprimir o cabeçalho do tabuleiro
+            printf ("%c ", tabela[i]);
+        }
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
-    return 0;
-}
+        printf ("\n");
+        
+        for (int i = 0; i < 10; i++) { // Esse for de fora é responsavel por imprimir os numeros que ficam na lateral do tabuleiro
+            printf ("%2d ", lateral[i]);
+            for (int j = 0; j < 10; j++) { // Esse for de dentro é responsavel pro preencher o interior do tabuleiro com 0
+            printf ("%d ", agua[i][j]);
+        }
+            printf ("\n");
+        }
+        return 0;
+    }
